aa2de738cb93f92d280d8a8b77702f4a750b26eb
Neal Makh <nealmakh@hotmail.co.uk>
2019-11-24 01:20:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ var chocolateBars = [
   'skittles'
 ]
 
-function addElementToBeginningOfArray(chocolateBars, 'eminems') {
+function addElementToBeginningOfArray('array, element') {
   return [...chocolateBars, 'eminems']
 }
